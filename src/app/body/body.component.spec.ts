8fe6cb117c3011fb56ca171148ea397f15c58d60
Geerocktricks <geerockface4@gmail.com>
2018-10-22 05:21:20 +0300

    This commit will initialize my nav component

--- src/app/body/body.component.spec.ts
+++ src/app/body/body.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BodyComponent } from './body.component';
 
-// describe('BodyComponent', () => {
+describe('BodyComponent', () => {
 //   let component: BodyComponent;
 //   let fixture: ComponentFixture<BodyComponent>;
 
